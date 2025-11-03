76e02333e06a5650e2980b08a4823e34a1a24885
Carly Orr <carly_wong_orr@bcit.ca>
2025-11-02 23:05:44 -0800

    Set link for each hike card to document ID
    Added a link to each hike's document ID in the card.

--- src/main.js
+++ src/main.js
@@ -144,6 +144,9 @@ async function displayCardsDynamically() {
             // 👇 ADD THIS LINE TO SET THE IMAGE SOURCE
             newcard.querySelector('.card-image').src = `./images/${hike.code}.jpg`;
 
+            // Add the link with the document ID
+            newcard.querySelector(".read-more").href = `eachHike.html?docID=${doc.id}`;
+
             // Attach the new card to the container
             document.getElementById("hikes-go-here").appendChild(newcard);
         });
